0f30b190334cb64b461ad143d007e2990612ae81
諦念 <keita0501513@gmail.com>
2025-08-11 16:05:24 +0900

    初回コミット

---
+++ neNmiNAtelier3/Assets/WorkSpace/Boot_UdonProgramSources/CustomWind.cs
@@ -0,0 +1,99 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[ExecuteInEditMode]
+public class CustomWind : UdonSharpBehaviour
+{
+    [Header("General Parameters")]
+    [Tooltip("Wind Speed in Kilometers per hour")]
+    public float WindSpeed = 30;
+    [Range(0.0f, 2.0f)]
+    [Tooltip("Wind Turbulence in percentage of wind Speed")]
+    public float Turbulence = 0.25f;
+
+
+    [Header("Noise Parameters")]
+    [Tooltip("Texture used for wind turbulence")]
+    public Texture2D NoiseTexture;
+    [Tooltip("Size of one world tiling patch of the Noise Texture, for bending trees")]
+    public float FlexNoiseWorldSize = 175.0f;
+    [Tooltip("Size of one world tiling patch of the Noise Texture, for leaf shivering")]
+    public float ShiverNoiseWorldSize = 10.0f;
+
+    [Header("Gust Parameters")]
+    [Tooltip("Texture used for wind gusts")]
+    public Texture2D GustMaskTexture;
+    [Tooltip("Size of one world tiling patch of the Gust Texture, for leaf shivering")]
+    public float GustWorldSize = 600.0f;
+
+    [Tooltip("Wind Gust Speed in Kilometers per hour")]
+    public float GustSpeed = 50;
+    [Tooltip("Wind Gust Influence on trees")]
+    public float GustScale = 1.0f;
+
+    Vector4 pos1 = new Vector4();
+    Vector4 pos2 = new Vector4();
+    Vector4 pos3 = new Vector4();
+    Vector4 pos4 = new Vector4();
+    Vector4 radius = new Vector4();
+
+    // Use this for initialization
+    void Start()
+    {
+        ApplySettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplySettings();
+    }
+
+    void OnValidate()
+    {
+        ApplySettings();
+    }
+
+    void ApplySettings()
+    {
+        VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_UdonWIND_SETTINGS_TexNoise"), NoiseTexture);
+        VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_UdonWIND_SETTINGS_TexGust"), GustMaskTexture);
+        VRCShader.SetGlobalVector(VRCShader.PropertyToID("_UdonWIND_SETTINGS_WorldDirectionAndSpeed"), GetDirectionAndSpeed());
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_FlexNoiseScale"), 1.0f / Mathf.Max(0.01f, FlexNoiseWorldSize));
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_ShiverNoiseScale"), 1.0f / Mathf.Max(0.01f, ShiverNoiseWorldSize));
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_Turbulence"), WindSpeed * Turbulence);
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_GustSpeed"), GustSpeed);
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_GustScale"), GustScale);
+        VRCShader.SetGlobalFloat(VRCShader.PropertyToID("_UdonWIND_SETTINGS_GustWorldScale"), 1.0f / Mathf.Max(0.01f, GustWorldSize));
+
+
+
+        pos1 = new Vector4(0, 0, 0, 0);
+        radius[0] = 0.1f;
+
+        pos2 = new Vector4(0, 0, 0, 0);
+        radius[1] = 0.1f;
+       
+        pos3 = new Vector4(0, 0, 0, 0);
+        radius[2] = 0.1f;
+       
+        pos4 = new Vector4(0, 0, 0, 0);
+        radius[3] = 0.1f;
+
+
+
+        VRCShader.SetGlobalMatrix(VRCShader.PropertyToID("_UdonWIND_SETTINGS_Points"), new Matrix4x4(pos1, pos2, pos3, pos4));
+        VRCShader.SetGlobalVector(VRCShader.PropertyToID("_UdonWIND_SETTINGS_Points_Radius"), radius);
+
+
+    }
+
+    Vector4 GetDirectionAndSpeed()
+    {
+        Vector3 dir = transform.forward.normalized;
+        return new Vector4(dir.x, dir.y, dir.z, WindSpeed * 0.2777f);
+    }
+}
---
+++ neNmiNAtelier3/Assets/neNneN/MemorialPhotoStand/Script/MemorialPhotoStand.cs
@@ -0,0 +1,162 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System;
+using Newtonsoft.Json.Linq;
+using VRC.Udon.Common.Interfaces;
+
+enum State
+{
+    Display,         // 表示中
+    Switching,       // 切替実行中
+}
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class MemorialPhotoStand : UdonSharpBehaviour
+{
+    [Header("▼触らない")]
+    public Renderer[] _renderer = null;
+    [Header("▼表示したい画像をドラッグして設定して下さい")]
+    public Texture[] _textures = null;
+    [Header("▼画像を表示する時間")]
+    public float _displaySeconds = 8.0f;
+    [Header("▼画像が切り替わるのにかかる時間")]
+    public float _fadeDuration = 2.0f;
+    
+    private float _fadeTimer = 0.0f;
+    private float _switchTimer = 0.0f;
+
+    State _currentState = State.Display;
+    [UdonSynced, FieldChangeCallback(nameof(CurrentTextureIndex))]
+    private int _currentTextureIndex = 0;
+    public int CurrentTextureIndex
+    {
+        get
+        {
+            return _currentTextureIndex;
+        }
+        set
+        {
+            _currentTextureIndex = value;
+
+            _currentState = State.Switching;
+            _fadeTimer = 0.0f;
+        }
+    }
+
+    private void Start()
+    {
+        if(_renderer != null && _renderer.Length >= 2)
+        {
+            _renderer[0].gameObject.SetActive(true);
+            _renderer[1].gameObject.SetActive(true);
+
+            _renderer[0].material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            _renderer[1].material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+
+        SwitchTexture();
+    }
+
+    private void Update() 
+    {
+        if(_renderer == null || _renderer.Length <= 0) { return; }
+        if(_textures == null || _textures.Length <= 0) { return; }
+
+
+        switch (_currentState)
+        {
+            case State.Display:
+                {// 表示中
+                    if (Networking.IsOwner(this.gameObject))
+                    {// 同期ホスト
+                        if (UpdateSwitch())
+                        {// 切替タイミング到達
+                            UpdateTextureIndex();
+                        }
+                    }
+                }
+                break;
+            case State.Switching:
+                {// フェード処理
+                    UpdateFade();
+                }
+                break;
+        }
+    }
+
+
+    /// <summary>
+    /// 画像切替の更新
+    /// </summary>
+    /// <returns>trueで切替タイミング</returns>
+    bool UpdateSwitch()
+    {
+        if (_textures.Length >= 2)
+        {
+            _switchTimer += Time.deltaTime;
+
+            if (_switchTimer > _displaySeconds)
+            {
+                Debug.Log($"[Memorial Photo Stand] Switch photo.");
+                _switchTimer = 0.0f;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void UpdateFade()
+    {
+        _fadeTimer += Time.deltaTime;
+        
+        if (_fadeTimer > _fadeDuration)
+        {
+            _renderer[0].material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            _renderer[1].material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+            this.SwitchTexture();
+            _currentState = State.Display;
+        }
+        else
+        {
+            float fValue = _fadeTimer / _fadeDuration;
+            
+            Mathf.Lerp(0.0f, 1.0f, fValue);
+
+            _renderer[0].material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f - fValue);
+            _renderer[1].material.color = new Color(1.0f, 1.0f, 1.0f, fValue);
+        }
+    }
+
+    /// <summary>
+    /// 表示する画像のINDEX更新
+    /// </summary>
+    void UpdateTextureIndex()
+    {
+        int nIndex = CurrentTextureIndex + 1;
+        CurrentTextureIndex = (nIndex >= _textures.Length) ? 0 : nIndex;
+
+        // 同期要求
+        RequestSerialization();
+    }
+
+    /// <summary>
+    /// 画像の更新
+    /// </summary>
+    void SwitchTexture()
+    {
+        int nIndex = CurrentTextureIndex;
+        int nNextIndex = CurrentTextureIndex + 1;
+        nNextIndex = (nNextIndex >= _textures.Length) ? 0 : nNextIndex;
+
+
+        var texture = _textures[nIndex];
+        var nextTexture = _textures[nNextIndex];
+
+        _renderer[0].material.mainTexture = texture;
+        _renderer[1].material.mainTexture = nextTexture;
+    }
+}
